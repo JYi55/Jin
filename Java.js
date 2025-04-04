4bb7ed9330f6eb0d17043b1136836af2e365ac81
JYi55 <jinyi.wong@yahoo.com>
2025-04-04 13:39:47 +0800

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,8 +1,23 @@
-// Function to scroll smoothly to the "work" section
+// Smooth scrolling for "Scroll" button
 function scrollToWork() {
-  const workSection = document.getElementById('work');
-  workSection.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
+  document.getElementById("services").scrollIntoView({ behavior: "smooth" });
 }
+
+// Event listener for the "Expand" button 
+document.addEventListener('DOMContentLoaded', function() {
+  const aboutSection2 = document.getElementById('about2');
+  const moreButton = document.getElementById('About2');
+
+  aboutSection2.style.display = 'none'; // Initially hide the about2 section
+
+  moreButton.addEventListener('click', function() { 
+      // Toggle the display state of the about2 section
+      if (aboutSection2.style.display === 'none') {
+          aboutSection2.style.display = 'flex';
+          moreButton.textContent = 'Close';
+      } else {
+          aboutSection2.style.display = 'none';
+          moreButton.textContent = 'Expand More';
+      }
+  });
+});
